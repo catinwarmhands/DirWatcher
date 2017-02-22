a89c2f77416603e1065bb9410f9a30154959c2c1
Кот Красивый <catinwarmhands@users.noreply.github.com>
2017-02-22 09:49:42 +0300

    Update dirwatcher.hpp

--- dirwatcher.hpp
+++ dirwatcher.hpp
@@ -12,27 +12,28 @@
 //  (c) 2017 by CIWH
 //  
 //  ###License
-//  public domain
+//  Public domain
 //  
 //  ##Basic usage:
 //  
 //  0. (Optional) You can define some actions before including: 
-//     `DIRWATCHER_FAILED_WATCH_DIR_ACTION` will be called if setting handle is failed, 
-//     `DIRWATCHER_FAILED_CLOSE_HANDLE_ACTION` will be called if closing hanle failed, 
-//     `DIRWATCHER_DEFAULT_CALLBACK_MESSAGE` will be inserted as code for default callback,
-//     `DIRWATCHER_MESSAGE_BUFFER_SIZE` is message buffer size (default is 1024 bytes)
+//   * `DIRWATCHER_FAILED_WATCH_DIR_ACTION` will be called if setting handle is failed, 
+//   * `DIRWATCHER_FAILED_CLOSE_HANDLE_ACTION` will be called if closing hanle failed, 
+//   * `DIRWATCHER_DEFAULT_CALLBACK_MESSAGE` will be inserted as code for default callback,
+//   * `DIRWATCHER_MESSAGE_BUFFER_SIZE` is message buffer size (default is 1024 bytes)
 //  1. `#include "path/to/this/file/dirwatcher.hpp"`
 //  2. Create object: `ciwh::DirWatcher watcher`; 
-//     Defaults: dir is `.`, non-recursive (dont watch subfolders)
+//   * Defaults: dir is `.`, non-recursive (dont watch subfolders)
 //  3. Set callback:  `
-//     watcher.setCallback([](ciwh::FileActionType type, const char* filename) {
-//     	   /*your code here*/
-//     });
+//  watcher.setCallback([](ciwh::FileActionType type, const char* filename) {
+//  	/*your code here*/
+//  });
 //  `
 //  4. Run: `watcher.start();`
 //  5. (Optional, will be called in destructor) Stop: `watcher.stop();`
 //  
-//  You can chande dir via `setDir(const char* path)` method, set recursive mode via `setRecursive(bool b)` method. If wather was running, it will restart.
+//  You can chande dir via `setDir(const char* path)` method, set recursive mode via `setRecursive(bool b)` method.
+//  If watcher was running, it will restart.
 //  
 //  Getters are: `bool isRecursive()`, `bool isRunning()`, `const char* const getDir()`
 
